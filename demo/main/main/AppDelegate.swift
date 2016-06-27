d56db958848eae4ac70c6521b3369d6fae04486a
rockgarden <rockgarden@sina.com>
2016-06-27 22:32:48 +0800

    add: menu

--- demo/main/main/AppDelegate.swift
+++ demo/main/main/AppDelegate.swift
@@ -15,15 +15,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
-        self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
-        // Override point for customization after application launch.
-        let mainViewController = RootViewController(nibName:nil,  bundle: nil)
-        let navigationViewController = UINavigationController(rootViewController: mainViewController)
-
-        self.window!.rootViewController = navigationViewController
-        //self.window!.backgroundColor = UIColor.whiteColor()
-        self.window!.makeKeyAndVisible()
+//        // Override point for customization after application launch.
+//        self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
+//        // Override point for customization after application launch.
+//        let mainViewController = RootViewController(nibName:nil,  bundle: nil)
+//        let navigationViewController = UINavigationController(rootViewController: mainViewController)
+//
+//        self.window!.rootViewController = navigationViewController
+//        //self.window!.backgroundColor = UIColor.whiteColor()
+//        self.window!.makeKeyAndVisible()
         return true
     }
 
