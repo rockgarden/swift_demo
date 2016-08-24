bc4d9e9711c27dec8552ade4e654ff7744fa7570
rockgarden <ro>
2016-08-24 10:58:12 +0800

    add

--- demo/NSTimer/NSTimer/FlipsideViewController.swift
+++ demo/NSTimer/NSTimer/FlipsideViewController.swift
@@ -2,48 +2,56 @@
 
 import UIKit
 
-protocol FlipsideViewControllerDelegate : class {
-    func flipsideViewControllerDidFinish(controller:FlipsideViewController)
+protocol FlipsideViewControllerDelegate: class {
+	func flipsideViewControllerDidFinish(controller: FlipsideViewController)
 }
 
 class FlipsideViewController: UIViewController {
-    
-    weak var delegate : FlipsideViewControllerDelegate!
-    
-    var timer : NSTimer!
-    
-    override func viewWillAppear(animated: Bool) {
-        super.viewWillAppear(animated)
+
+	weak var delegate: FlipsideViewControllerDelegate!
+    var cancelableTimer : CancelableTimer!
+	var timer: NSTimer!
+
+	override func viewWillAppear(animated: Bool) {
+		super.viewWillAppear(animated)
+		print("starting timer")
+		self.timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: #selector(dummy), userInfo: nil, repeats: true)
+		self.timer.tolerance = 0.1
+        
+        self.cancelableTimer = CancelableTimer(once: false) {
+            [unowned self] in // comment out this line to leak
+            self.dummy()
+        }
         print("starting timer")
-        self.timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: #selector(dummy), userInfo: nil, repeats: true)
-        self.timer.tolerance = 0.1
-    }
+        self.cancelableTimer.startWithInterval(1)
+	}
     
-    func dummy(t:NSTimer) {
-        print("timer fired")
-    }
-    
-    override func viewDidDisappear(animated: Bool) {
-        super.viewDidDisappear(animated)
-         return; // uncomment and we will leak
-        print("invalidate")
-        self.timer?.invalidate()
-    }
-    
-    @IBAction func done (sender:AnyObject!) {
-        print("done")
-        self.delegate?.flipsideViewControllerDidFinish(self)
-    }
-    
-    // if deinit is not called when you tap Done, we are leaking
-    deinit {
-        print("deinit")
-    }
-    
+	func dummy() {
+		print("timer fired")
+	}
+
+	override func viewDidDisappear(animated: Bool) {
+		super.viewDidDisappear(animated)
+//		return; // uncomment and we will leak
+		print("invalidate")
+		self.timer?.invalidate()
+	}
+
+	@IBAction func done (sender: AnyObject!) {
+		print("done")
+		self.delegate?.flipsideViewControllerDidFinish(self)
+	}
+
+	// if deinit is not called when you tap Done, we are leaking
+	deinit {
+		print("deinit")
+        self.cancelableTimer?.cancel()
+	}
+
 }
 
-extension FlipsideViewController : UIBarPositioningDelegate {
-    func positionForBar(bar: UIBarPositioning) -> UIBarPosition {
-        return .Top
-    }
+extension FlipsideViewController: UIBarPositioningDelegate {
+	func positionForBar(bar: UIBarPositioning) -> UIBarPosition {
+		return .Top
+	}
 }
--- demo/NSTimer/NSTimer/MainViewController.swift
+++ demo/NSTimer/NSTimer/MainViewController.swift
@@ -2,16 +2,16 @@ import UIKit
 
 class MainViewController: UIViewController, FlipsideViewControllerDelegate {
 
-    func flipsideViewControllerDidFinish(controller:FlipsideViewController) {
-        self.dismissViewControllerAnimated(true, completion: nil)
-    }
-    
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        if segue.identifier == "showAlternate" {
-            if let dest = segue.destinationViewController as? FlipsideViewController {
-                dest.delegate = self
-            }
-        }
-    }
-    
+	func flipsideViewControllerDidFinish(controller: FlipsideViewController) {
+		self.dismissViewControllerAnimated(true, completion: nil)
+	}
+
+	override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+		if segue.identifier == "showAlternate" {
+			if let dest = segue.destinationViewController as? FlipsideViewController {
+				dest.delegate = self
+			}
+		}
+	}
+
 }
