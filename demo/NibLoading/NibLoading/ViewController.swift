8f05bd5807d6ac2f3175a561e7dd7b109b73e581
rockgarden <rockgarden@sina.com>
2016-10-27 22:02:11 +0800

    add

---
+++ demo/NibLoading/NibLoading/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  NibLoading
+//
+//  Created by wangkan on 2016/10/27.
+//  Copyright © 2016年 rockgarden. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var coolview : UIView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let arr = Bundle.main.loadNibNamed("View", owner: nil, options: nil)
+        let v0 = arr?[0] as! UIView
+        let v1 = arr?[1] as! UIView
+        self.view.addSubview(v0)
+        self.view.addSubview(v1)
+
+        Bundle.main.loadNibNamed("CoolView", owner: self, options: nil)
+        v0.addSubview(self.coolview)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
