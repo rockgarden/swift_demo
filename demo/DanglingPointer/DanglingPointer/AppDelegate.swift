3855f4ace350d49b883e61e742fadc154881f375
rockgarden <rockgarden@sina.com>
2016-11-02 23:11:25 +0800

    add

---
+++ demo/DanglingPointer/DanglingPointer/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  DanglingPointer
+//
+//  Created by wangkan on 2016/11/2.
+//  Copyright © 2016年 rockgarden. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
+
