7ba1fc7d484d4c1e60f6d9662c966eab58eca2b7
rockgarden <rockgarden@sina.com>
2016-11-23 22:20:10 +0800

    add

---
+++ demo/SQLite/SQLite/FMDBVC.swift
@@ -0,0 +1,60 @@
+//
+//  FMDBVC.swift
+//  SQLite
+//
+//  Created by wangkan on 2016/11/23.
+//  Copyright © 2016年 rockgarden. All rights reserved.
+//
+
+import UIKit
+
+// recast using pure Objective-C interface to FMDB, so that we are not dependent
+// on any particular incarnation of Swift in FMDB itself
+
+class FMDBVC: UIViewController {
+
+    var dbpath : String {
+        get {
+            let docsdir = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).last!
+            return (docsdir as NSString).appendingPathComponent("people.db")
+        }
+    }
+
+    @IBAction func creatDB (_ sender: Any!) {
+
+        do {
+            let fm = FileManager.default
+            try fm.removeItem(atPath: self.dbpath) // in case we did this once already
+        } catch {}
+
+        guard let db = FMDatabase(path:self.dbpath), db.open()
+            else {print("Ooooops"); return}
+
+        db.executeUpdate("create table people (lastname text, firstname text)", withArgumentsIn:[])
+
+        db.beginTransaction()
+
+        db.executeUpdate("insert into people (firstname, lastname) values (?,?)", withArgumentsIn:["Matt", "Neuburg"])
+        db.executeUpdate("insert into people (firstname, lastname) values (?,?)", withArgumentsIn:["Snidely", "Whiplash"])
+        db.executeUpdate("insert into people (firstname, lastname) values (?,?)", withArgumentsIn:["Dudley", "Doright"])
+        db.commit()
+
+        print("I think I created it")
+        db.close()
+
+    }
+
+    @IBAction func searchDB (_ sender: Any!) {
+        guard let db = FMDatabase(path:self.dbpath), db.open()
+            else {print("Ooooops"); return}
+
+        if let rs = db.executeQuery("select * from people", withArgumentsIn:[]) {
+            while rs.next() {
+                print(rs["firstname"], rs["lastname"])
+            }
+        }
+        db.close()
+        
+    }
+    
+}
---
+++ demo/SQLite/SQLite/SQLite-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  SQLite-Bridging-Header.h
+//  SQLite
+//
+//  Created by wangkan on 2016/11/23.
+//  Copyright © 2016年 rockgarden. All rights reserved.
+//
+
+#ifndef SQLite_Bridging_Header_h
+#define SQLite_Bridging_Header_h
+
+#import "FMDB.h"
+
+#endif /* SQLite_Bridging_Header_h */
