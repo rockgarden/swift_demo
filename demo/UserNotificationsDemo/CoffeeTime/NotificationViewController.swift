1cfef3b42358ba9b28b532df03615832b8769570
rockgarden <rockgarden@sina.com>
2016-12-23 22:24:20 +0800

    add

---
+++ demo/UserNotificationsDemo/CoffeeTime/NotificationViewController.swift
@@ -0,0 +1,42 @@
+
+import UIKit
+import UserNotifications
+import UserNotificationsUI
+
+extension CGSize {
+    init(_ width:CGFloat, _ height:CGFloat) {
+        self.init(width:width, height:height)
+    }
+}
+
+/* Info.plist must contain these keys:
+ UNNotificationExtensionCategory
+ UNNotificationExtensionInitialContentSizeRatio
+ optional: UNNotificationExtensionDefaultContentHidden
+*/
+//FIXME: where can see the UI
+class NotificationViewController: UIViewController, UNNotificationContentExtension {
+
+    @IBOutlet var label: UILabel?
+    @IBOutlet weak var imageView: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.preferredContentSize = CGSize(320, 80)
+    }
+
+    func didReceive(_ notification: UNNotification) {
+        let req = notification.request
+        let content = req.content
+        let atts = content.attachments
+        if let att = atts.first, att.identifier == "cup" {
+            if att.url.startAccessingSecurityScopedResource() { // system has copy!
+                if let data = try? Data(contentsOf: att.url) {
+                    self.imageView.image = UIImage(data: data)
+                }
+                att.url.stopAccessingSecurityScopedResource()
+            }
+        }
+        self.view.setNeedsLayout() // seems to help things along
+    }
+}
