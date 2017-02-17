58214d0e619f2ea4c0eb22fb52ce0e210ee73f73
rockgarden <rockgarden@sina.com>
2017-02-17 22:25:32 +0800

    add new

---
+++ demo/FXFormsSwiftDemo/FXFormsSwiftDemo/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  FXFormsSwiftDemo
+//
+//  Created by wangkan on 2017/2/17.
+//  Copyright © 2017年 rockgarden. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+}
+
---
+++ demo/FXFormsSwiftDemo/FXFormsSwiftDemo/FXFormsSwiftDemo-Bridging-Header.h
@@ -0,0 +1,10 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#ifndef FXForms_FXFormsSwiftDemo_Bridging_Header_h
+#define FXForms_FXFormsSwiftDemo_Bridging_Header_h
+
+#import "FXForms.h"
+
+#endif
