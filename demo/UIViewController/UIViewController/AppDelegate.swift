46c849a2164cf8795a1c0f471e4a34b9b306b6e4
rockgarden <rockgarden@sina.com>
2017-04-10 20:31:23 +0800

    add

---
+++ demo/UIViewController/UIViewController/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  UIViewController
+//
+//  Created by wangkan on 2017/4/8.
+//  Copyright © 2017年 rockgarden. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
+
