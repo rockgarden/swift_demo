283214a1fcf91ce4743be6dd2df318ee5db0e556
rockgarden <ro>
2017-09-04 16:27:51 +0800

    add

--- demo/UIAnimator/UIAnimator/UIViewPropertyAnimator_Fling.swift
+++ demo/UIAnimator/UIAnimator/UIViewPropertyAnimator_Fling.swift
@@ -57,6 +57,52 @@ class UIViewPropertyAnimator_Fling: UIViewController {
         which = which < 2 ? which+1 : 1
     }
     
+    func demo() {
+        var animator = UIViewPropertyAnimator(duration: 1.0, curve: .easeOut){
+            self.v.center = CGPoint(0.5, 0.2)
+        }
+        animator.startAnimation()
+        
+        /// 两个控制点定义的贝塞尔曲线
+        animator = UIViewPropertyAnimator(
+            duration: 1.0,
+            controlPoint1: CGPoint(0.1,0.5),
+            controlPoint2: CGPoint(0.5, 0.2)){
+                self.v.alpha = 0.0
+            }
+        
+        /// 弹簧效果
+       animator = UIViewPropertyAnimator(
+            duration: 1.0,
+            dampingRatio:0.4){ //dampingRatio阻尼系数
+                self.v.center = CGPoint(x:0, y:0)
+        }
+        animator.startAnimation(afterDelay:2.5)
+    }
+    
+    /// 我们可以通过调用`startAnimation`, `pauseAnimation` 和 `stopAnimation`轻松地与动画流交互。
+    func blockDemo() {
+        
+        //`UIViewPropertyAnimator` 采用的是能够为动画器提供很多有趣能力的`UIViewImplicitlyAnimating`协议。例如，除了在初始化时候指定的block外，你还可以指定多个动画block。
+        // Initialization
+        let animator = UIViewPropertyAnimator(duration: 2.0, curve: .easeOut){
+            self.v.alpha = 0.0
+        }
+        // Another animation block
+        animator.addAnimations{
+            self.v.center = CGPoint(0.5, 0.2)
+        }
+        animator.startAnimation()
+        
+        /// 动画的默认流（从起始点到结束点），可以通过`fractionComplete`属性更改。这个值表示动画完成的百分比，取值范围是0 到 1。你能够修改这个值来像你期望的那样驱动流（例如：用户可能会用滑块或滑动手势实时地修改`fraction`)。
+        //animator.fractionComplete = slider.value
+        
+        /// `position`参数是一个 `UIViewAnimatingPosition`类型的值，它有三个枚举值，分别代表动画是在开始停止，结束后停止，还是当前位置停止。 通常你都会收到结束的枚举值。
+        animator.addCompletion { (position) in
+            print("Animation completed")
+        }
+    }
+    
 }
 
 
